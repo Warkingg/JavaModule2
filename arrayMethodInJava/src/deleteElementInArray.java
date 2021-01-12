546ab54106893b62f0ac778a93f4b0ccc43a13af
Warkingg <phucryanlee@gmail.com>
2021-01-12 15:22:30 +0700

    deleteElementInArray

---
+++ arrayMethodInJava/src/deleteElementInArray.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class deleteElementInArray {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = {4, 12, 7, 8, 1, 6, 9};
+        int[] newArr = null;
+        System.out.print("Enter Element to be deleted : ");
+        int x = in.nextInt();
+        for(int i = 0; i < arr.length; i++){
+        if(arr[i] == x){
+                newArr = new int[arr.length - 1];
+                for(int index = 0; index < i; index++){
+                    newArr[index] = arr[index];
+                }
+                for(int j = i; j < arr.length - 1; j++){
+                    newArr[j] = arr[j+1];
+                }
+                break;
+            }
+        }
+            System.out.println("The new array after " + x + " was deleted :");
+            for (int i = 0; i < newArr.length; i++) {
+                System.out.print(" " + newArr[i]);
+            }
+    }
+}
--- arrayMethodInJava/src/minInArrayMethod.java
+++ arrayMethodInJava/src/minInArrayMethod.java
@@ -7,9 +7,8 @@ public class minInArrayMethod {
     }
     public static int minValue(int[] arr){
         int min = arr[0];
-        int i ;
-        int index = 1;
-        for (i = 0 ; i < arr.length; i++) {
+        int index = 0;
+        for (int i = 0 ; i < arr.length; i++) {
             if (arr[i] < min) {
                 min = arr[i];
                 index = i;
